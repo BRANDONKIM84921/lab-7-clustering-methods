57334902aef78467260dba7f737f54b855e0fe97
BRANDONKIM84921 <81550902+BRANDONKIM84921@users.noreply.github.com>
2023-05-31 20:30:16 -0700

    bug fixed k_means

--- R/k_means.R
+++ R/k_means.R
@@ -8,39 +8,45 @@
 #' @return list with cluster means, cluster assignments and sum of squares for each cluster
 k_means <- function(df, k, PCA = F) {
 
-    clust_ind <- sample(k, 1:nrow(df))
+    clust_ind <- sample(1:nrow(df), k)
     clusters <- list()
     for (i in 1:length(clust_ind)) {
         clusters[[i]] <- as.numeric(df[clust_ind[i],])
     }
-    new_clusters <- list()
+    old_clusters <- list()
+    df_clusters <- df
 
-    while(!set_equal(new_clusters, clusters)) {
-        df_assign <- df %>%
-            mutate(cluster = {
-                for (cluster in clusters) {
-                    distances <- append(distances, sqrt(sum((as.numeric(df[i,]) - cluster) ^ 2)))
-                }
-                which.min(distances)
-            })
+    while(!setequal(old_clusters, clusters)) {
 
-        clusters <- new_clusters
+        old_clusters <- clusters
+        centers <- c()
+        cmeans <- list()
 
-        df_center <- df_assign %>%
+        for (i in 1:nrow(df)) {
+            distances <- c()
+            for (j in 1:length(clusters)) {
+                distances <- append(distances, sqrt(sum((as.numeric(df[i,]) - clusters[[j]]) ^ 2)))
+            }
+            centers <- append(centers, which.min(distances))
+        }
+
+        df_clusters$cluster <- centers
+
+        df_center <- df_clusters %>%
             group_by(cluster) %>%
             summarize(across(everything(), mean))
 
-        new_clusters <- as.list(as.data.frame(t(df_center[,-1])))
+        clusters <- as.list(as.data.frame(t(df_center[,-1])))
     }
 
     ss <- rep(0, k)
 
-    for (i in 1:length(df)) {
-        ss[df_assign$cluster[i]] <-
-            ss[df_assign$cluster[i]] + sqrt(sum((as.numeric(df[i,]) - clustering_means[i]) ^ 2))
+    for (i in 1:nrow(df)) {
+        ss[df_clusters$cluster[i]] <-
+            ss[df_clusters$cluster[i]] + sum((as.numeric(df[i,]) - clusters[[df_clusters$cluster[i]]]) ^ 2)
     }
 
-    return(list(cluster_means = new_clusters,
-                clustering_vector = df_assign$cluster,
+    return(list(cluster_means = unname(clusters),
+                clustering_vector = df_clusters$cluster,
                 sum_of_squares = ss))
 }

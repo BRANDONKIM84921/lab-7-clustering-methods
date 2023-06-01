5f4583c8482b784bde1891f618fa9ccb01757ff9
BRANDONKIM84921 <81550902+BRANDONKIM84921@users.noreply.github.com>
2023-05-31 21:53:01 -0700

    Update test-k_means.R

--- tests/testthat/test-k_means.R
+++ tests/testthat/test-k_means.R
@@ -1,3 +1,15 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("simple case", {
+    set.seed(101)
+
+    test <- data.frame(x = c(19, 8, 13, 52, 2, 13, 15), y = c(1, 13, 12, 43, 1, 2, 17))
+
+    expected <- kmeans(test, 2)
+
+    actual <- k_means(test, 2)
+
+    expect_equal(actual$clustering_vector, expected$cluster)
+
+    expect_equal(actual$cluster_means, as.data.frame(expected$centers))
+
+    expect_equal(actual$sum_of_squares, expected$withinss)
 })
